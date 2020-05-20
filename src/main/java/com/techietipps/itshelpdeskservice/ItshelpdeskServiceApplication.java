cdd315472f3ddb64b541145293159c9fcef35abe
Naveen <naveenanem22@gmail.com>
2020-05-20 16:37:39 +0530

    configuring the rabbit mq for consumer

--- src/main/java/com/techietipps/itshelpdeskservice/ItshelpdeskServiceApplication.java
+++ src/main/java/com/techietipps/itshelpdeskservice/ItshelpdeskServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ItshelpdeskServiceApplication {
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItshelpdeskServiceApplication.class, args);
--- src/main/java/com/techietipps/itshelpdeskservice/services/TicketServiceImpl.java
+++ src/main/java/com/techietipps/itshelpdeskservice/services/TicketServiceImpl.java
@@ -5,20 +5,25 @@ import java.util.List;
 import com.techietipps.itshelpdeskservice.dao.TicketDao;
 import com.techietipps.itshelpdeskservice.models.Ticket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service("ticketServiceImpl")
-public class TicketServiceImpl implements TicketService {
+public class TicketServiceImpl implements TicketService, MessageListener {
 
     @Autowired
     @Qualifier("ticketDaoImpl")
     private TicketDao ticketDao;
 
-    @Autowired
-    private RestTemplate restTemplate;
+    
+
+    private final Logger logger = LoggerFactory.getLogger(TicketServiceImpl.class);
 
     @Override
     public List<Ticket> getTickets() {
@@ -34,6 +39,12 @@ public class TicketServiceImpl implements TicketService {
     @Override
     public void updateTicket(Ticket ticket) {
         ticketDao.updateTicket(ticket);
+
+    }
+
+    @Override
+    public void onMessage(Message message) {
+       logger.info("Received message: "+message.toString());
 
     }
 
